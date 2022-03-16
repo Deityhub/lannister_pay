8063a009ac0140ddb9deaee9da3f58d43077b80b
Ojini Chizoba Jude <judoc96@gmail.com>
2022-03-16 14:19:36 +0100

    memoize transaction fee computation per transaction id

--- routes/feeCompute.js
+++ routes/feeCompute.js
@@ -4,9 +4,18 @@ const Path = require("path");
 const computeTransactionFee = require("../utils/computeTransactionFee");
 const { readFileContent } = require("../utils/fsHandlers");
 
+const transactionFeeStore = new Map();
+
 router.post("/compute-transaction-fee", async (req, res) => {
   try {
     const payload = req.body;
+
+    if (transactionFeeStore.has(payload.ID)) {
+      // grab already calculated value from store
+      const transactionFee = transactionFeeStore.get(payload.ID);
+      return res.status(200).send(transactionFee);
+    }
+
     const filePath = Path.resolve(process.cwd(), "feeSpec.json");
 
     const fileContent = await readFileContent(filePath);
@@ -18,6 +27,8 @@ router.post("/compute-transaction-fee", async (req, res) => {
         Error: "No fee configuration for the transaction.",
       });
     }
+
+    transactionFeeStore.set(payload.ID, transactionFee);
 
     return res.status(200).send(transactionFee);
   } catch (error) {
