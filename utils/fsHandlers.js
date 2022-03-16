0ed06b22cc20f3e3b818838bf5958dd3e3014909
Ojini Chizoba Jude <judoc96@gmail.com>
2022-03-16 12:56:30 +0100

    setup utils to handle read/write fs operations

---
+++ utils/fsHandlers.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+/**
+ * a promise based utility to read content of a file
+ * @param {string} filePath path to file
+ * @returns promise
+ */
+function readFileContent(filePath) {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(filePath)) {
+      fs.readFile(filePath, { encoding: "utf8" }, function (error, data) {
+        if (error) {
+          reject(error);
+        }
+        resolve(data);
+      });
+    }
+  });
+}
+
+/**
+ * a promise based utility to write content to file
+ * @param {string} filePath path to file
+ * @param {string} content content to be written in the file
+ * @returns promise
+ */
+function writeToFile(filePath, content) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, content, { encoding: "utf-8" }, function (error) {
+      if (error) {
+        reject(error);
+      }
+      resolve("Content saved successfully");
+    });
+  });
+}
+
+module.exports = {
+  readFileContent,
+  writeToFile,
+};
